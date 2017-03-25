b62ca14338dbc9d973626ea7ec237e4e02000ed0
Alejandro Grajales Alvarez <Alejandro Grajales Alvarez>
2017-03-24 20:30:28 -0500

    conexion base de datos

--- connection.php
+++ connection.php
@@ -1,6 +1,12 @@
 <?php
-/*
+$serverName = "173.163.175.227"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"spreasheet", "UID"=>"sa", "PWD"=>"Insolsap1");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
- conexion
-*/
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
 ?>
