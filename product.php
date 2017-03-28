9a6690e8dc47118c51f9556b50cc922840c05900
Alejandro Grajales Alvarez <Alejandro Grajales Alvarez>
2017-03-28 01:06:57 -0500

    insertqr

--- product.php
+++ product.php
@@ -1,6 +1,7 @@
 <?php
 	error_reporting(0);
 	require('connection.php');
+
 	$postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     @$purl = $request->purl;
@@ -9,17 +10,15 @@
     //echo $purl;
     $hoy = getdate();
     $date=$hoy["year"]."-".$hoy["mon"]."-".$hoy["mday"];
-    $sql1 = "INSERT INTO data (qr, dateqr)
-         VALUES (?, ?)";
-	$params1 = array($purl, $date);
-	if(sqlsrv_query( $conn, $sql1, $params1 )){
+    $sql1 = "INSERT INTO `data`(`dateqr`, `qr`) VALUES ('$date','$purl')";
+    if(mysqli_query( $connection, $sql1)){
 		echo "<br> ------------ <br> Se hizo <br>---------------<br>";
 	}else{
-		echo "<br> ------------ <br> NO se hizo <br>---------------<br>";
+		echo "<br> ------------ <br> No se hizo <br>---------------<br>";
 	}
-    $sql = "SELECT qr,dateqr FROM data";
-    $stmt = sqlsrv_query( $conn, $sql);
-    while( $obj = sqlsrv_fetch_object( $stmt)) {
+    $sql = "SELECT `qr`, `dateqr` FROM `data`";
+    $stmt = mysqli_query( $connection, $sql);
+    while( $obj = mysql_fetch_object( $stmt)) {
     	echo "QR: ".$obj->qr." Date:".date_format($obj->dateqr,"y-m-d")."<br>";
 	}
 	
